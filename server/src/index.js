f99beebb85e87a5e53b561f852bf54b7b52c99f0
julianstauta <julianstauta@gmail.com>
2020-02-26 17:52:57 -0500

    Export2

--- server/src/index.js
+++ server/src/index.js
@@ -14,4 +14,4 @@ app.use('/api/names', names);
 
 const port  = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, '192.168.56.3', () => console.log(`Server started on port ${port}`));
